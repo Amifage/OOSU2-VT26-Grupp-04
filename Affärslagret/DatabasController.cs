42e0cffe33edd72ec37e77771894212c4a6c4555
Amanda Fagerström <s2408076@student.hb.se>
2026-02-10 13:49:21 +0100

    ny klass oh försök till koppling

---
+++ Affärslagret/DatabasController.cs
@@ -0,0 +1,14 @@
+﻿using Datalager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Affärslagret
+{
+    public class DatabasController
+    {
+        UnitOfWork();
+    }
+}
