102bc23e1e250a7e00804e7d35e022314f8ecb73
Gabbelabbe03 <gabriel03arnesson@gmail.com>
2026-02-12 13:03:18 +0100

    validering

--- Affärslagret/PersonalController.cs
+++ Affärslagret/PersonalController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datalager;
+using Entitetslager;
 
 namespace Affärslagret
 {
     public class PersonalController
     {
-
+       
     }
 }
