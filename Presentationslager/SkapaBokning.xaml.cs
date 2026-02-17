a9e109a415f242de235750f7699f3730627d777d
Amanda Fagerström <s2408076@student.hb.se>
2026-02-17 12:39:17 +0100

    små fix bokning

--- Presentationslager/SkapaBokning.xaml.cs
+++ Presentationslager/SkapaBokning.xaml.cs
@@ -69,8 +69,7 @@ namespace Presentationslager
 
             if (lediga.Any())
             {
-                SparaBokningButton.IsEnabled = true;
-                MessageBox.Show($"Hittade {lediga.Count} lediga resurser.");
+                SparaBokningButton.IsEnabled = true;             
             }
             else
             {
@@ -118,7 +117,6 @@ namespace Presentationslager
                 MessageBox.Show("Ett fel uppstod: " + ex.Message);
             }
         }
-
         // Din befintliga metod för att skapa DateTime från valen i fönstret
         private DateTime? GetBokningsDateTime()
         {
