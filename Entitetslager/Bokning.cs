fb01afc97c90b1d558b8261910f9cb34c77631d0
Amanda Fagerström <s2408076@student.hb.se>
2026-02-17 12:22:09 +0100

    Skapa bokning done

--- Entitetslager/Bokning.cs
+++ Entitetslager/Bokning.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entitetslager
 {
     public class Bokning
     {
         [Key] public int BokningsID { get; set; }
-
+        [Column("MedlemsID")] // Säg till EF att kolumnen i SQL faktiskt heter MedlemsID
         public int MedlemID { get; set; }
         public virtual Medlem medlem {  get; set; }
 
