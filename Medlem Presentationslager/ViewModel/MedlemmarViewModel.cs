7f79819381400e0ec2ee1a811cbc6b672aec6f14
Amanda Fagerström <s2408076@student.hb.se>
2026-03-06 13:02:47 +0100

    fixxx

--- Medlem Presentationslager/ViewModel/MedlemmarViewModel.cs
+++ Medlem Presentationslager/ViewModel/MedlemmarViewModel.cs
@@ -1,14 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Affärslagret;
+using Entitetslager;
 
 namespace Medlem_Presentationslager.ViewModel
 {
-    public class MedlemmarViewModel 
+    public class MedlemmarViewModel : INotifyPropertyChanged
     {
+        private readonly MedlemController _medlemController;
+        private ObservableCollection<Medlem> _medlemmar;
 
+        public ObservableCollection<Medlem> Medlemmar
+        {
+            get => _medlemmar;
+            set
+            {
+                _medlemmar = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MedlemmarViewModel()
+        {
+            _medlemController = new MedlemController();
+            LaddaMedlemmar();
+        }
+
+        private void LaddaMedlemmar()
+        {
+            // Hämtar alla medlemmar från affärslagret och lägger i kollektionen
+            var lista = _medlemController.HämtaAllaMedlemmar();
+            Medlemmar = new ObservableCollection<Medlem>(lista);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
