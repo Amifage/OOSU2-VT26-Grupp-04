1b8fb791dca2f5e268931bed5fc6c2699b5e4677
Gabbelabbe03 <gabriel03arnesson@gmail.com>
2026-03-06 10:41:07 +0100

    mvvm menymedlem

---
+++ Medlem Presentationslager/ViewModel/MenyMedlemViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Medlem_Presentationslager.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Medlem_Presentationslager.ViewModel
+{
+    public class MenyMedlemViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand SkapaBokningCommand { get; set; }
+        public ICommand MinaSidorCommand { get; set; }
+        public ICommand AndraAvbokaCommand { get; set; }
+        public ICommand BokningshistorikCommand { get; set; }
+        public ICommand LoggaUtCommand { get; set; }
+
+        public MenyMedlemViewModel()
+        {
+            SkapaBokningCommand = new RelayCommand(OpenSkapaBokning);
+            MinaSidorCommand = new RelayCommand(OpenMinaSidor);
+            AndraAvbokaCommand = new RelayCommand(OpenAndraAvboka);
+            BokningshistorikCommand = new RelayCommand(OpenBokningshistorik);
+            LoggaUtCommand = new RelayCommand(LoggaUt);
+        }
+
+        private void OpenSkapaBokning()
+        {
+            SkapaMedlem window = new SkapaMedlem();
+            window.Show();
+
+            StangAktivtFonster();
+        }
+
+        private void OpenMinaSidor()
+        {
+            MinaSidor window = new MinaSidor();
+            window.Show();
+
+            StangAktivtFonster();
+        }
+
+        private void OpenAndraAvboka()
+        {
+            MessageBox.Show("Fönster för Ändra/Avboka är inte skapat ännu.");
+        }
+
+        private void OpenBokningshistorik()
+        {
+            MessageBox.Show("Fönster för Bokningshistorik är inte skapat ännu.");
+        }
+
+        private void LoggaUt()
+        {
+            MedlemLogin window = new MedlemLogin();
+            window.Show();
+
+            StangAktivtFonster();
+        }
+
+        private void StangAktivtFonster()
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.IsActive)
+                {
+                    window.Close();
+                    break;
+                }
+            }
+        }
+    
+    }
+}
