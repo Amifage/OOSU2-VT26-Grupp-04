3030308fdc323789340a89bbd221d0d15655a0ce
Amanda Fagerström <s2408076@student.hb.se>
2026-03-06 16:52:20 +0100

    mina sidor done

--- Medlem Presentationslager/ViewModel/MinaSidorViewModel.cs
+++ Medlem Presentationslager/ViewModel/MinaSidorViewModel.cs
@@ -1,25 +1,44 @@
-﻿
-           using System;
-            using System.Windows;
-            using System.Windows.Input;
-            using System.Windows.Media.Imaging;
-            using Microsoft.Win32;
-            using Entitetslager;
-            using Affärslagret;
-            using Medlem_Presentationslager.Command;
-            using System.ComponentModel;
-            using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using Entitetslager;
+using Affärslagret;
+using Medlem_Presentationslager.Command;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Medlem_Presentationslager.ViewModel
 {
-        public class MinaSidorViewModel : INotifyPropertyChanged
-        {
-            private readonly MedlemController _medlemController;
-            private Medlem _inloggadMedlem;
-        private string _medlemsNivå;
+    public class MinaSidorViewModel : INotifyPropertyChanged
+    {
+        private readonly MedlemController _medlemController;
+        private Medlem _inloggadMedlem;
+        private string _valdBonnivå;
         private string _nyttLösenord;
 
-       
+        public Medlem InloggadMedlem
+        {
+            get => _inloggadMedlem;
+            set { _inloggadMedlem = value; OnPropertyChanged(); }
+        }
+
+        public List<string> MedlemsNivåer { get; } = new List<string> { "Företag", "Fast", "Flex" };
+
+        public string MedlemsNivå 
+        {
+            get => _valdBonnivå;
+            set
+            {
+                _valdBonnivå = value;
+                if (InloggadMedlem != null)
+                {
+
+                    InloggadMedlem.Medlemsnivå = value;
+                }
+                OnPropertyChanged();
+            }
+        }
 
         public string NyttLösenord
         {
@@ -27,45 +46,17 @@ namespace Medlem_Presentationslager.ViewModel
             set { _nyttLösenord = value; OnPropertyChanged(); }
         }
 
-        public Medlem InloggadMedlem
-            {
-                get => _inloggadMedlem;
-                set { _inloggadMedlem = value; OnPropertyChanged(); }
-            }
-        public List<string> MedlemsNivåer { get; } = new List<string>
-        {
-            "Företag",
-            "Fast",
-            "Flex"
-        };
-       
-        public string MedlemsNivå
-        {
-            get => _medlemsNivå;
-            set
-            {
-                _medlemsNivå = value;
-                // VIKTIGT: Uppdatera objektet som faktiskt sparas i databasen
-                if (InloggadMedlem != null)
-                {
-                    InloggadMedlem.Medlemsnivå = value;
-                }
-                OnPropertyChanged();
-            }
-        }
-
         public ICommand SparaÄndringarCommand { get; }
-            public ICommand TillbakaCommand { get; }
+        public ICommand TillbakaCommand { get; }
 
         public MinaSidorViewModel(Medlem medlem)
         {
             _medlemController = new MedlemController();
             InloggadMedlem = medlem;
 
-            // Sätt utgångsvärdet för ComboBoxen baserat på medlemmens nuvarande status
+            // Viktigt: Sätt startvärdet så ComboBoxen visar rätt nivå direkt
             MedlemsNivå = medlem.Medlemsnivå;
 
-            // Kommandon
             SparaÄndringarCommand = new RelayCommand(SparaÄndringar);
             TillbakaCommand = new RelayCommand(Tillbaka);
         }
@@ -74,56 +65,36 @@ namespace Medlem_Presentationslager.ViewModel
         {
             try
             {
-                // 1. Hantera E-post (Säkerställ små bokstäver)
+                // Formatera e-post
                 if (!string.IsNullOrEmpty(InloggadMedlem.Epost))
-                {
                     InloggadMedlem.Epost = InloggadMedlem.Epost.Trim().ToLower();
-                }
 
-                // 2. Hantera nytt lösenord från TextBox-bindningen
+                // Hantera nytt lösenord
                 if (!string.IsNullOrWhiteSpace(NyttLösenord))
                 {
-                    // Uppdatera medlemmens lösenord och tvinga till små bokstäver för inloggningsmatchning
                     InloggadMedlem.Lösenord = NyttLösenord.Trim().ToLower();
-
-                    // Rensa fältet i gränssnittet efter lyckad tilldelning
                     NyttLösenord = string.Empty;
                 }
 
-         
-
-                // 3. Spara till databas via controllern
+                // Spara objektet där Medlemsnivå nu är korrekt satt
                 _medlemController.UppdateraMedlem(InloggadMedlem);
-
                 MessageBox.Show("Dina uppgifter har uppdaterats!");
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Kunde inte spara: " + ex.Message);
+                MessageBox.Show("Kunde inte spara: " + ex.InnerException?.Message);
             }
         }
 
         private void Tillbaka(object obj)
         {
-            // Nu kan vi skicka tillbaka rätt medlem till menyn!
             MenyMedlem meny = new MenyMedlem(_inloggadMedlem);
             meny.Show();
-            StängFönster(obj);
-        }
-
-        private void StängFönster(object parameter)
-        {
-            if (parameter is Window fönster)
-            {
-                fönster.Close();
-            }
+            if (obj is Window window) window.Close();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-            protected void OnPropertyChanged([CallerMemberName] string name = null) =>
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-    
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
-    
-
