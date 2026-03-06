b43fb4e3fe34220d834f40649e3fc5a831a48930
Amanda Fagerström <s2408076@student.hb.se>
2026-03-06 14:43:53 +0100

    nya klass

---
+++ Medlem Presentationslager/ViewModel/MinaSidorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medlem_Presentationslager.ViewModel
+{
+    public class MinaSidorViewModel
+    {
+        public MinaSidorViewModel() 
+        {
+
+        }
+    }
+}
